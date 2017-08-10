e0e7b565488223f45aae6186a2d096172fc479cc
EJ Vickery <email": null>
2017-08-09 18:40:52 -0700

    Set up basic routes for students, teams, teachers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,38 @@
 class StudentsController < ApplicationController
+  before_action :get_student, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @student = Student.update(student_params)
+  end
+
+  def destroy
+  end
+
+  def get_student
+    @student = Student.find(params[:id])
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:first_name, :last_name, :hero_name, :superpower, :gender, :grade_level, :shirt_size)
+    end
+
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,38 @@
 class TeamsController < ApplicationController
+  before_action :get_teams, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(teacher_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @team = Team.update(teacher_params)
+  end
+
+  def destroy
+  end
+
+  def get_teacher
+    @team = Team.find(params[:id])
+  end
+
+  private
+    def team_params
+      params.require(:team).permit(:name, :teacher_id)
+    end
+
 end
