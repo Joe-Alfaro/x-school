9c7b1c4abcafb52fd10314439b4d5732564373d3
Andrea Scott <email": null>
2017-08-10 17:34:27 -0700

    Update app html to hide profile if user is not logged in. Add space between teacher and student login forms.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Changes from Andrea for Devise build
-  root to: "home#index"
-  #_login paths from Mark, need testing
+  root to: "teams#index"
   devise_for :students
   devise_for :teachers
 
